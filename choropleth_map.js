0452ed0fb4b0fb52902c4215cc904d7a8eaa62fa
jhan0058 <jhan0058@student.monash.edu>
2024-09-22 09:43:28 +0800

    Initial Uploads

---
+++ choropleth_map.js
@@ -0,0 +1,7 @@
+var choropleth_map = "choropleth_map.json";
+
+vegaEmbed("#choropleth_map", choropleth_map)
+  .then(function (result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+  })
+  .catch(console.error);
